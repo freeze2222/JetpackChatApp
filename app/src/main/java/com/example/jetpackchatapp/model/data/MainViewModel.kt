0b4c31cf008344370ad5671805a99b0eaf68f65f
freeze2222 <opop1094@gmail.com>
2022-12-19 20:49:14 +0300

    Nothing again

--- app/src/main/java/com/example/jetpackchatapp/model/data/MainViewModel.kt
+++ app/src/main/java/com/example/jetpackchatapp/model/data/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.jetpackchatapp.model.data
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackchatapp.model.ChatModel
@@ -7,7 +9,12 @@ import com.example.jetpackchatapp.model.MessageModel
 import com.example.jetpackchatapp.repository.setListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainViewModel : ViewModel() {
     var value:Any? = null
@@ -22,4 +29,40 @@ class MainViewModel : ViewModel() {
         }
 
     }
+}
+
+fun ReadText(): SnapshotStateList<String> {
+    val _messages = mutableStateListOf<MessageModel>()
+    try {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.reference.child("messages")
+
+        myRef.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val value = snapshot.getValue(HashMap::class.java)
+                if (value != null){
+                    _messages.add(value.values.)
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
+    }
+    return _messages
 }
